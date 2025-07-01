6ecae758c61e55c28188248ea235f041e48ec106
Sorad-Ai <soradai707@gmail.com>
2025-07-01 12:29:53 +0545

    build files

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ declare module "@remix-run/node" {
 export default defineConfig({
   plugins: [
     remix({
-      ssr: true,
+      ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
